48cf209b505c52c49a9f1ece9bda3ce83142d52a
ge75xiq <ge75xiq@mytum.de>
2020-11-09 11:18:29 +0100

    Added my photo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/me.jpg">
     <HelloWorld msg="Hien Nguyen"/>
   </div>
 </template>
