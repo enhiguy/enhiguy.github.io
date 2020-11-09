7aed8af930e46f6df7d2fe43a59a0ee3b73630ba
ge75xiq <ge75xiq@mytum.de>
2020-11-09 11:57:54 +0100

    Image and Title changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,3 +16,25 @@ export default {
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  max-width: 50%;
+  height: auto;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
